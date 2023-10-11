edbe5f8d9734be1728d13efcd5399cbd9078cd58
Krampusisko <grzegoz2004@gmail.com>
2023-10-11 14:03:29 +0200

    udalo

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const client = new MongoClient(mongoString);
 async function main() {
     try {
     await client.connect();
+    await listDB(client);
     } catch (error) {
         console.error(error);
     } finally {
         await client.close();
      }
     
+}
+async function listDB(client) {
+    let databaseList = await client.db().admin().listDatabases();
+    console.log("Lista baz danych:");
+    databaseList.databases.forEach(database => {
+        console.log("Nazwa: " + database.name + ", Rozmiar: " + database.sizeOnDisk);
+    });
 }
 main();
